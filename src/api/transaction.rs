8de3a077018fd0da708b4784ce02771cdd1fa143
Kave Bahraman <sadegh.b218@gmail.com>
2022-07-26 20:41:01 +0430

    Adds api methods for transaction

--- src/api/transaction.rs
+++ src/api/transaction.rs
@@ -1,18 +1,20 @@
 use super::establish_connection;
 use super::DatabaseResult;
 use crate::authentication::gaurd;
-use crate::models::CurrencyType;
+use crate::db::DbConn;
+use crate::models::{CurrencyType, NewTransaction, Transaction};
+use diesel::PgConnection;
 use rocket::serde::json::Json;
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 pub struct TransactionData {
-    kind: bool,
-    source: Option<String>,
-    value: String,
-    currency: Option<CurrencyType>,
-    username: String,
-    bank_account: i32,
+    pub kind: bool,
+    pub title: String,
+    pub value: String,
+    pub currency: CurrencyType,
+    pub user_id: String,
+    pub bank_account: i32,
 }
 
 // admin has no control on user data
@@ -23,20 +25,71 @@ pub struct TransactionData {
     format = "application/json",
     data = "<new_transaction>"
 )]
-pub fn create_transaction(new_transaction: Json<TransactionData>, user: gaurd::UserGaurd) {}
+pub fn create_transaction(
+    new_transaction: Json<TransactionData>,
+    user: gaurd::UserGaurd,
+    mut conn: DbConn,
+) -> Option<Json<Transaction>> {
+    let trans = new_transaction.0;
+    if let DatabaseResult::Succeful(trans) = Transaction::add(&mut conn, &trans.into()) {
+        Some(Json(trans))
+    } else {
+        None
+    }
+}
 
 /// Get to retrieve a transaction
 #[get("/transaction/<identifier>")]
-pub fn get_transaction(identifier: i32, user: gaurd::UserGaurd) {}
+pub fn get_transaction(
+    identifier: i32,
+    user: gaurd::UserGaurd,
+    mut conn: DbConn,
+) -> Option<Json<Transaction>> {
+    if let DatabaseResult::Succeful(trans) = Transaction::get(&mut conn, identifier) {
+        Some(Json(trans))
+    } else {
+        None
+    }
+}
 
 /// Get to retrieve an account's all transactions
 #[get("/transaction?<account_id>")]
-pub fn get_account_all_transactions(account_id: i32, user: gaurd::UserGaurd) {}
+pub fn get_account_all_transactions(
+    account_id: i32,
+    user: gaurd::UserGaurd,
+    mut conn: DbConn,
+) -> Option<Json<Vec<Transaction>>> {
+    if let DatabaseResult::Succeful(trans_vec) = Transaction::all(&mut conn, account_id) {
+        Some(Json(trans_vec))
+    } else {
+        None
+    }
+}
 
 /// Delete to remove a transaction
 #[delete("/transaction/<identifier>")]
-pub fn delete_transaction(identifier: i32, user: gaurd::UserGaurd) {}
+pub fn delete_transaction(
+    identifier: i32,
+    user: gaurd::UserGaurd,
+    mut conn: DbConn,
+) -> Option<Json<Transaction>> {
+    if let DatabaseResult::Succeful(trans) = Transaction::delete(&mut conn, identifier) {
+        Some(Json(trans))
+    } else {
+        None
+    }
+}
 
 /// Delete to remove an account's all transactions
 #[delete("/transaction?<account_id>")]
-pub fn delete_account_all_transactions(account_id: i32, user: gaurd::UserGaurd) {}
+pub fn delete_account_all_transactions(
+    account_id: i32,
+    user: gaurd::UserGaurd,
+    mut conn: DbConn,
+) -> Option<Json<Vec<Transaction>>> {
+    if let DatabaseResult::Succeful(trans_vec) = Transaction::delete_all(&mut conn, account_id) {
+        Some(Json(trans_vec))
+    } else {
+        None
+    }
+}
