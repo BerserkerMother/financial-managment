46c3eb542118056ef4ba7dd52b31fb7ee37aea7e
Kave Bahraman <sadegh.b218@gmail.com>
2022-07-26 10:44:03 +0430

    Completes Bearer Token auth

--- src/authentication/mod.rs
+++ src/authentication/mod.rs
@@ -1,39 +1,90 @@
 pub mod hasher;
 pub mod token_generator;
+use crate::establish_connection;
 pub use token_generator::random_token;
 
+use crate::models::result_variant::DatabaseResult;
+use crate::models::User;
+use rocket::http::Status;
 use rocket::http::{Cookie, CookieJar};
 use rocket::outcome::{IntoOutcome, Outcome};
 use rocket::request::{self, FromRequest, Request};
 use std::convert::Infallible;
 
+#[derive(Debug)]
+pub enum GaurdError {
+    NotAdmin,
+}
+
 pub mod gaurd {
     use super::*;
     #[derive(Debug)]
-    pub struct AdminGaurd(String);
+    pub struct AdminGaurd {
+        pub username: String,
+    }
 
     #[rocket::async_trait]
     impl<'r> FromRequest<'r> for AdminGaurd {
-        type Error = Infallible;
+        type Error = GaurdError;
 
         async fn from_request(req: &'r Request<'_>) -> request::Outcome<AdminGaurd, Self::Error> {
-            req.cookies()
-                .get_private("username")
-                .and_then(|coockie| Some(String::from(coockie.value())))
-                .map(|s| AdminGaurd(s))
-                .or_forward(())
+            let mut values: Vec<_> = req.headers().get("Authorization").collect();
+            if values.len() != 1 {
+                return Outcome::Forward(());
+            }
+            let mut header_fileds = values[0].split(' ');
+            match header_fileds.next() {
+                Some(method) if method == "Bearer" => (),
+                _ => return Outcome::Forward(()),
+            }
+            let token = match header_fileds.next() {
+                Some(token) => token,
+                _ => return Outcome::Forward(()),
+            };
+
+            let mut conn = establish_connection();
+            match User::get_by_token(&mut conn, token) {
+                DatabaseResult::Succeful(user) if user.role == true => {
+                    Outcome::Success(AdminGaurd {
+                        username: user.username,
+                    })
+                }
+                _ => return Outcome::Failure((Status::BadRequest, GaurdError::NotAdmin)),
+            }
         }
     }
 
     #[derive(Debug)]
-    pub struct UserGaurd(String);
-
+    pub struct UserGaurd {
+        pub username: String,
+    }
     #[rocket::async_trait]
     impl<'r> FromRequest<'r> for UserGaurd {
         type Error = Infallible;
 
         async fn from_request(req: &'r Request<'_>) -> request::Outcome<UserGaurd, Self::Error> {
-            Outcome::Success(UserGaurd(String::from("BerserkerMother")))
+            let mut values: Vec<_> = req.headers().get("Authorization").collect();
+            if values.len() != 1 {
+                return Outcome::Forward(());
+            }
+            let mut values = values[0].split(' ');
+            match values.next() {
+                Some(method) if method == "Bearer" => (),
+                _ => return Outcome::Forward(()),
+            };
+
+            let token = match values.next() {
+                Some(token) => token,
+                _ => return Outcome::Forward(()),
+            };
+
+            let mut conn = establish_connection();
+            match User::get_by_token(&mut conn, token) {
+                DatabaseResult::Succeful(user) => Outcome::Success(UserGaurd {
+                    username: user.username,
+                }),
+                _ => Outcome::Forward(()),
+            }
         }
     }
 }
